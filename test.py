dc4ce43356e9ab95c2491d46d26d2cb79b85d091
Yosri <yosri.miledi@supcom.tn>
2021-04-02 20:03:30 +0100

    Project Init

---
+++ test.py
@@ -0,0 +1,11 @@
+class Seance :
+    def __init__(self,groupId,duration,profId,matiere,hDebut,hFin):
+        self.groupId=groupId
+        self.duree=duration
+        self.profId=profId
+        self.matiere=matiere
+        self.hDebut=hDebut
+        self.hFin=hFin
+    
+
+    def 
